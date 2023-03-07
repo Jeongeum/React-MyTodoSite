ee97715dedd2b93c627a33a9ad3802fdf0dd9118
Jeongeum <wjddma1202@naver.com>
2023-03-07 15:52:02 +0900

    Feat : 전역 스타일링 theme 파일 추가

---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+const fontSizes = {
+  xs: "10px",
+  s: "12px",
+  base: "16px",
+  l: "20px",
+  xl: "40px",
+};
+
+const colors = {
+  colorMain: "#EFEFEF",
+  colorSkyBlue: "#E6F4FE",
+  colorBlue: "#0057BC",
+  colorDisabled: "#C4C4C4",
+  colorWhite: "#FFFFFF",
+  colorRed: "#FC585A",
+  colorDiRed: "#FE9090",
+};
+
+const lightTheme = {
+  fontSizes,
+  colors,
+};
+
+export default lightTheme;
